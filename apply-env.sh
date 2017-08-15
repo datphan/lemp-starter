040c7dda4b864788816f3e8f00b9110904b8ed77
Dat Phan <datphan0110@gmail.com>
2017-08-15 11:24:55 +0700

    rebuild

--- apply-env.sh
+++ apply-env.sh
@@ -8,6 +8,6 @@ printenv NGINX_PORT
 
 export NGINX_PORT=$NGINX_PORT
 
-envsubst < /etc/nginx/conf.d/default.conf.tpl > /etc/nginx/conf.d/default.conf
+envsubst '${NGINX_PORT}' < /etc/nginx/conf.d/default.conf.tpl > /etc/nginx/conf.d/default.conf
 
 nginx -g "daemon off;"
