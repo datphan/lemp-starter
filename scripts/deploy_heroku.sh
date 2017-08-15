531da80c23880bc3f70951dd54e56cd9bab64924
Dat Phan <datphan0110@gmail.com>
2017-08-15 07:36:03 +0700

    rebuild

--- scripts/deploy_heroku.sh
+++ scripts/deploy_heroku.sh
@@ -7,6 +7,8 @@ chmod 600 ~/.netrc # Heroku cli complains about permissions without this
 
 heroku whoami
 
+heroku config:get PORT
+
 docker pull $APP_PROD_IMAGE
 docker tag $APP_PROD_IMAGE $HEROKU_IMAGE
 docker push $HEROKU_IMAGE
